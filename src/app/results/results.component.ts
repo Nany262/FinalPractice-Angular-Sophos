a3438ca0912ebb18f895d2c205ca522378fc5a5c
Daniela Zuluaga <dzuluag@bancolombia.com.co>
2020-10-07 08:53:01 -0500

    Se agrega mensaje de padre a hijo

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,25 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { User } from '../models/user.model';
 import { RandomUserService } from '../service/random-user.service';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.css']
+  styleUrls: ['./results.component.css'],
 })
 export class ResultsComponent implements OnInit {
+  public users: User[] = [];
+  public user: User;
 
-  public users:User[]=[];
-  public user:User;
-  constructor(public service:RandomUserService) { }
+  @Input() numberOfUsers;
+
+  constructor(public service: RandomUserService) {}
 
   ngOnInit(): void {
-    this.service.getInformation().subscribe(res=>this.getDataUsers(res))
+    this.service
+      .getInformation(this.numberOfUsers)
+      .subscribe((res) => this.getDataUsers(res));
   }
 
-  getDataUsers(res){
-    for (let item of res.results){
-      this.user=new User(
+  getDataUsers(res) {
+    for (let item of res.results) {
+      this.user = new User(
         item.name.first,
         item.name.last,
         item.dob.age,
@@ -27,10 +31,10 @@ export class ResultsComponent implements OnInit {
         item.location.city,
         item.location.country,
         item.email,
-        item.phone);
+        item.phone
+      );
       this.users.push(this.user);
     }
-    console.log(this.users)
+    console.log(this.users);
   }
-
 }
--- src/app/service/random-user.service.ts
+++ src/app/service/random-user.service.ts
@@ -5,15 +5,14 @@ import { Observable } from 'rxjs';
 import { User } from '../models/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RandomUserService {
+  public urlApi: string = environment.urlApi;
 
-  public urlApi:string=environment.urlApi+"9"; 
+  constructor(public http: HttpClient) {}
 
-  constructor(public http:HttpClient) { }
-
-  public getInformation():Observable<User>{
-    return this.http.get<User>(this.urlApi);
+  public getInformation(numberOfUsers): Observable<User> {
+    return this.http.get<User>(this.urlApi + numberOfUsers);
   }
 }
